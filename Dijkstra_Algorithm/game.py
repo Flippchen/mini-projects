f40ed730736870ad2b0aede8c2c56c53631c185b
Flippchen <91947480+Flippchen@users.noreply.github.com>
2023-02-22 19:25:00 +0100

    first commit for Dijkstra game. Added Box Class

---
+++ Dijkstra_Algorithm/game.py
@@ -0,0 +1,46 @@
+from tkinter import messagebox, Tk
+import pygame
+import sys
+
+window_width = 500
+window_height = 500
+
+window = pygame.display.set_mode((window_width, window_height))
+
+columns = 25
+rows = 25
+
+box_width = window_width // columns
+box_height = window_height // rows
+
+grid = []
+queue = []
+path = []
+
+
+class Box:
+    def __init__(self, i, j):
+        self.x = i
+        self.y = j
+        self.start = False
+        self.wall = False
+        self.target = False
+        self.queued = False
+        self.visited = False
+        self.neighbours = []
+        self.prior = None
+
+    def draw(self, win, color):
+        pygame.draw.rect(win, color, (self.x * box_width, self.y * box_height, box_width - 2, box_height - 2))
+
+    def set_neighbours(self):
+        if self.x > 0:
+            self.neighbours.append(grid[self.x - 1][self.y])
+        if self.x < columns - 1:
+            self.neighbours.append(grid[self.x + 1][self.y])
+        if self.y > 0:
+            self.neighbours.append(grid[self.x][self.y - 1])
+        if self.y < rows - 1:
+            self.neighbours.append(grid[self.x][self.y + 1])
+
+
