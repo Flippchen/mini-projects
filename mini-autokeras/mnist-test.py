9c9a1433c1d2e4d195d27c4244218b0a3fe0c8c3
Flippchen <91947480+Flippchen@users.noreply.github.com>
2023-05-30 21:02:25 +0200

    added autokeras example

---
+++ mini-autokeras/mnist-test.py
@@ -0,0 +1,31 @@
+import numpy as np
+from tensorflow import keras
+import autokeras as ak
+import tensorflow as tf
+
+
+# Model / data parameters
+num_classes = 10
+input_shape = (28, 28, 1)
+
+# Load the data and split it between train and test sets
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+# Scale images to the [0, 1] range
+x_train = x_train.astype("float32") / 255
+x_test = x_test.astype("float32") / 255
+# Make sure images have shape (28, 28, 1)
+x_train = np.expand_dims(x_train, -1)
+x_test = np.expand_dims(x_test, -1)
+print("x_train shape:", x_train.shape)
+print(x_train.shape[0], "train samples")
+print(x_test.shape[0], "test samples")
+
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+with tf.device('/GPU:0'):
+    clf = ak.ImageClassifier()
+    clf.fit(x_train, y_train)
+    results = clf.predict(x_test)
