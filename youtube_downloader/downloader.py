257b24787f3a9c68cb54d900862eeb2cf57610a5
Flippchen <91947480+Flippchen@users.noreply.github.com>
2023-09-23 18:34:47 +0200

    added resolution

--- youtube_downloader/downloader.py
+++ youtube_downloader/downloader.py
@@ -5,13 +5,17 @@ from pytube import YouTube
 
 def download_video():
     url = url_entry.get()
+    resolution = resolution_var.get()
     download_folder = "downloads"
     if not os.path.exists(download_folder):
         os.makedirs(download_folder)
 
     try:
         yt = YouTube(url)
-        stream = yt.streams.filter(progressive=True, file_extension='mp4').first()
+        stream = yt.streams.filter(res=resolution, progressive=True, file_extension='mp4').first()
+        if not stream:
+            download_label.config(text=f"No stream available for {resolution} resolution.")
+            return
         download_label.config(text=f"Downloading {yt.title}...")
         stream.download(download_folder)
         download_label.config(text=f"Download complete! Saved in {download_folder} folder.")
@@ -27,6 +31,13 @@ url_label.pack(pady=10)
 
 url_entry = Entry(root, width=50)
 url_entry.pack(pady=10)
+
+# Drop-down menu for resolution selection
+resolution_var = StringVar(root)
+resolution_var.set("720p")
+resolutions = ["144p", "240p", "360p", "480p", "720p", "1080p"]
+resolution_menu = OptionMenu(root, resolution_var, *resolutions)
+resolution_menu.pack(pady=10)
 
 download_button = Button(root, text="Download Video", command=download_video)
 download_button.pack(pady=20)
