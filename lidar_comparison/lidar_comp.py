1e0905bf1dc29d2fe1142dd347a85650192d108b
Flippchen <91947480+Flippchen@users.noreply.github.com>
2023-11-15 20:11:22 +0100

    Feature: init all angles

--- lidar_comparison/lidar_comp.py
+++ lidar_comparison/lidar_comp.py
@@ -2,3 +2,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from tabulate import tabulate
+
+
+def plot_lines_triangles_shadows(num_lines, field_of_view_degrees, line_color, num_triangles, triangle_height, triangle_width, plot_name, angle_offset=0):
+    # Set the origin for all lines
+    origin = [0, 1.3]  # Move the origin to the left
+
+    # Convert field of view to radians
+    field_of_view_radians = np.radians(field_of_view_degrees)
+
+    # Generate angles for the lines
+    angles = np.linspace(-field_of_view_radians / 2, field_of_view_radians / 2, num_lines)
+
+    # Set a scaling factor for the line length
+    scaling_factor = 40.0
+
+    # Set a larger figure size
+    plt.figure(figsize=(50, 10))
+
+    # Initialize a list to store the number of lines hitting each cone
+    cone_hits = [0] * num_triangles
