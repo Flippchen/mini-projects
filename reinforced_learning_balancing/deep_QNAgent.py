1746f28dc3ddda4974ce0c6f437a9460b69bb49a
Flippchen <91947480+Flippchen@users.noreply.github.com>
2024-01-03 01:48:34 +0100

    feature: Added model creation

--- reinforced_learning_balancing/deep_QNAgent.py
+++ reinforced_learning_balancing/deep_QNAgent.py
@@ -10,6 +10,20 @@ from rl.agents import DQNAgent
 from rl.policy import BoltzmannGumbelQPolicy
 from rl.memory import SequentialMemory
 
+
+def create_model(states, actions):
+    model = Sequential(
+        [
+            Flatten(input_shape=(1, states)),
+            Dense(24, activation='relu'),
+            Dense(24, activation='relu'),
+            Dense(actions, activation='linear')
+        ]
+    )
+    model.summary()
+    return model
+
+
 # tf.compat.v1.disable_eager_execution() activate if needed
 env = gym.make('CartPole-v1')
 
