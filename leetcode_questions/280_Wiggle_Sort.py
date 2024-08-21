e13955c8e55ffd9db17cbb4e16fc7d47e4030033
Flippchen <91947480+Flippchen@users.noreply.github.com>
2024-08-21 10:51:13 +0200

    Add solution for leetcode problem

--- leetcode_questions/280_Wiggle_Sort.py
+++ leetcode_questions/280_Wiggle_Sort.py
@@ -38,3 +38,5 @@ class Solution(object):
             if (i % 2 == 0 and nums[i] > nums[i + 1]) or\
                (i % 2 == 1 and nums[i] < nums[i + 1]):
                 nums[i], nums[i + 1] = nums[i + 1], nums[i]
+
+# Modified on 2024-09-01 14:19:56.519243
